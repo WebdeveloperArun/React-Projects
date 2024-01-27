cda1b6182f09d9733aff1ebd4a22248dbb0136ad
Arun Kumar <143159338+WebdeveloperArun@users.noreply.github.com>
2024-01-27 05:08:48 +0000

    error fix

--- Blog-Write/src/App.jsx
+++ Blog-Write/src/App.jsx
@@ -1,18 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import {useDispatch} from 'react-redux'
 import './App.css'
+import authService from './appwrite/auth'
+import {login, logout} from './store/authslice'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+    const [loading, setloading] = useState(true)
+    const dispatch = useDispatch()
 
-  return (
-    <>
-    <h1> My blog app with appwrite</h1>
-    </>
-    
-  )
+    useEffect(()=>{
+      authService.getCurrentUser()
+      .then((userData)=>{
+        if (userData) {
+          dispatch(login({userData}))
+        } else{
+          dispatch(logout())
+        }
+      })
+      .finally(()=> setloading(false))
+
+    } ,[])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+          <main>
+            Hallo
+          </main>
+        <Footer />
+      </div>
+    </div>
+  ): null
 }
 
 export default App
